33602829231edd9870938de0d01dad36e1282f6f
Lembani Sakala <lembanisakala@gmail.com>
2022-05-04 14:25:11 +0200

    Check if the string is at least 1 character long and not longer than 10 characters

--- task-one.js
+++ task-one.js
@@ -1,4 +1,7 @@
 const stringLength = (str) => {
+  if (str.length < 1 && str.length > 10) {
+    throw Error(`${str} is too short or too long`);
+  }
   const len = str.split('').length;
   return len;
 };
