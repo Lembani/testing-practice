b32bf1f3f54492919843a9bea8ed6bd626185d3b
Lembani Sakala <lembanisakala@gmail.com>
2022-05-04 14:27:05 +0200

    Add test to check if the string is at least 1 character long and not longer than 10 characters

--- task-one.test.js
+++ task-one.test.js
@@ -1,5 +1,11 @@
 const stringLength = require('./task-one.js');
 
-test('length of string', () => {
-  expect(stringLength('bob')).toBe(3);
+test('character count is greater than 0', () => {
+  const str = 'bob';
+  expect(stringLength(str)).toBeGreaterThan(0);
+});
+
+test('character count is less than or equal to 10', () => {
+  const str = 'bob';
+  expect(stringLength(str)).toBeLessThanOrEqual(10);
 });
